63b71c67a42b6ad898ac01243493c43966c8a9fd
hb9901 <hb9901@gmail.com>
2024-07-23 16:36:16 +0900

    feat: Label 컴포넌트 초기 기능 설정

---
+++ components/Label/Label.tsx
@@ -0,0 +1,46 @@
+import { StrictPropsWithChildren } from "@/types/common";
+import { cva, VariantProps } from "class-variance-authority";
+import clsx from "clsx";
+import { HTMLAttributes } from "react";
+
+const LabelClass = cva("", {
+  variants: {
+    variant: {
+      label2: "text-[12px]",
+      label1: "text-[10px]",
+    },
+    weight: {
+      bold: "font-bold",
+      semibold: "font-semibold",
+    },
+  },
+  defaultVariants: {},
+});
+
+type LabelProps = {
+  htmlFor: string;
+  className?: string;
+} & StrictPropsWithChildren<
+  VariantProps<typeof LabelClass> & HTMLAttributes<HTMLLabelElement>
+>;
+
+function Label({
+  htmlFor,
+  variant,
+  weight,
+  className,
+  children,
+  ...props
+}: LabelProps) {
+  return (
+    <label
+      htmlFor={htmlFor}
+      className={clsx(LabelClass({ variant, weight }), className)}
+      {...props}
+    >
+      {children}
+    </label>
+  );
+}
+
+export default Label;
