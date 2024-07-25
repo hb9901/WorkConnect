1c00d723cbca640f8b0bf07aeac105900cac2025
hb9901 <hb9901@gmail.com>
2024-07-25 13:03:02 +0900

    feat: 프로필 및 마이페이지 폴더구조 및 컴포넌트 기본 구조 생성

--- next.config.mjs
+++ next.config.mjs
@@ -1,37 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [
-        {
-          loader: "@svgr/webpack",
-          options: {
-            svgoConfig: {
-              plugins: [
-                {
-                  name: "preset-default",
-                  params: {
-                    overrides: {
-                      removeViewBox: false,
-                      addAttributesToSVGElement: {
-                        params: {
-                          attributes: [
-                            { preserveAspectRatio: "xMidYMid meet" },
-                          ],
-                        },
-                      },
-                    },
-                  },
-                },
-              ],
-            },
-          },
-        },
-      ],
-    });
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
