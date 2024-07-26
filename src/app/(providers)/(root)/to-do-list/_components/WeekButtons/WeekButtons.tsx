a9e7bdbf667a277c46926ae43e5508982601eff2
hb9901 <hb9901@gmail.com>
2024-07-26 16:02:48 +0900

    style: 날짜 네비게이션 디자인 수정

--- src/app/(providers)/(root)/to-do-list/_components/WeekButtons/WeekButtons.tsx
+++ src/app/(providers)/(root)/to-do-list/_components/WeekButtons/WeekButtons.tsx
@@ -6,17 +6,19 @@ const WeekButtons = () => {
   const { selectedDate, handleClickNextWeek, handleClickPreviousWeek } = useDateStore();
   const year = selectedDate.year();
   const month = selectedDate.month() + 1;
-  const day = selectedDate.date();
 
   return (
-    <div>
-      <strong>
-        {year}.{month}.{day}
-      </strong>
-      <div>
-        <button onClick={handleClickPreviousWeek}>{`<`}</button>
-        <button onClick={handleClickNextWeek}>{`>`}</button>
+    <div className="flex flex-row justify-between mb-5">
+      <div className="flex flex-row gap-4">
+        <strong>
+          {year}.{month}
+        </strong>
+        <div>
+          <button onClick={handleClickPreviousWeek}>{`<`}</button>
+          <button onClick={handleClickNextWeek}>{`>`}</button>
+        </div>
       </div>
+      <button className="text-slate-500 text-sm">오늘</button>
     </div>
   );
 };
--- src/app/(providers)/(root)/to-do-list/page.tsx
+++ src/app/(providers)/(root)/to-do-list/page.tsx
@@ -5,22 +5,21 @@ import Header from './_components/Header';
 import ToDoList from './_components/ToDoList';
 import WeekButtons from './_components/WeekButtons';
 import WeekDate from './_components/WeekDate';
-import WeekName from './_components/WeekName';
 
 const ToDoListPage = () => {
   const { todoList }: { todoList: Tables<'todo'>[] } = useTodo();
-  const beforeTodoList = todoList.filter((todo) => todo.status === '진행 전');
-  const progressTodoList = todoList.filter((todo) => todo.status === '진행 중');
-  const completedTodoList = todoList.filter((todo) => todo.status === '진행 완료');
+  const beforeTodoList = todoList && todoList.filter((todo) => todo.status === '진행 전');
+  const progressTodoList = todoList && todoList.filter((todo) => todo.status === '진행 중');
+  const completedTodoList = todoList && todoList.filter((todo) => todo.status === '진행 완료');
 
   return (
     <>
       <header>
         <Header />
       </header>
-      <main>
+      <main className="mt-5 px-4">
         <WeekButtons />
-        <WeekName />
+
         <WeekDate />
         <ToDoList title="진행 전" todoList={beforeTodoList} />
         <ToDoList title="진행 중" todoList={progressTodoList} />
