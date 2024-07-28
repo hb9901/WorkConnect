7629169844b4e7ef79d0b9006e393d92a27520fc
riverSun1 <haeweon0426@gmail.com>
2024-07-29 04:31:53 +0900

    feat: Input 컴포넌트 디자인 반영

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export interface InputProps {
   type?: string;
@@ -7,17 +7,221 @@ export interface InputProps {
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   className?: string;
   id: string;
+  status?: 'default' | 'error' | 'success';
+  togglePasswordVisibility?: boolean;
 }
 
-const Input = ({ id, type = 'text', value, className, ...props }: InputProps) => {
+const Input = ({
+  id,
+  type = 'text',
+  value,
+  className,
+  status = 'default',
+  togglePasswordVisibility = false,
+  onChange,
+  ...props
+}: InputProps) => {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
+  const [state, setState] = useState<'default' | 'focus' | 'typing'>('default');
+
+  useEffect(() => {
+    setState(value ? 'typing' : 'default');
+  }, [value]);
+
+  const handleIconClick = () => {
+    if (togglePasswordVisibility === false) {
+      setIsPasswordVisible((prev) => !prev);
+    }
+  };
+
+  const handleFocus = () => {
+    setIsFocused(true);
+    setState('focus');
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+    if (value) {
+      setState('typing');
+    } else {
+      setState('default');
+    }
+  };
+
+  const handleClickDelete = () => {
+    const event = {
+      target: { value: '' }
+    } as React.ChangeEvent<HTMLInputElement>;
+    onChange(event);
+  };
+
+  const renderIcon = () => {
+    if (status === 'error') {
+      return (
+        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <g clipPath="url(#clip0)">
+            <path
+              d="M8 5.333V8M8 10.667H8.007M14.667 8C14.667 11.682 11.682 14.667 8 14.667C4.318 14.667 1.333 11.682 1.333 8C1.333 4.318 4.318 1.333 8 1.333C11.682 1.333 14.667 4.318 14.667 8Z"
+              stroke="#FD4E39"
+              strokeWidth="1.6"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </g>
+          <defs>
+            <clipPath id="clip0">
+              <rect width="16" height="16" fill="white" />
+            </clipPath>
+          </defs>
+        </svg>
+      );
+    } else if (status === 'success') {
+      return (
+        <svg width="14" height="10" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M12.334 1L5 8.333 1.667 5"
+            stroke="#05AC4B"
+            strokeWidth="1.6"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      );
+    } else {
+      switch (state) {
+        case 'focus':
+          return (
+            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <g clipPath="url(#clip0)">
+                <path
+                  d="M0.667 8C0.667 8 3.333 2.667 8 2.667C12.667 2.667 15.333 8 15.333 8C15.333 8 12.667 13.333 8 13.333C3.333 13.333 0.667 8 0.667 8Z"
+                  stroke="#2F323C"
+                  strokeWidth="1.6"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M8 10C9.105 10 10 9.105 10 8C10 6.895 9.105 6 8 6C6.896 6 6 6.895 6 8C6 9.105 6.896 10 8 10Z"
+                  stroke="#2F323C"
+                  strokeWidth="1.6"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </g>
+              <defs>
+                <clipPath id="clip0">
+                  <rect width="16" height="16" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+          );
+        case 'typing':
+          if (type === 'password') {
+            return (
+              <svg
+                onClick={handleIconClick}
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g clipPath="url(#clip0)">
+                  <path
+                    d="M0.667 8C0.667 8 3.333 2.667 8 2.667C12.667 2.667 15.333 8 15.333 8C15.333 8 12.667 13.333 8 13.333C3.333 13.333 0.667 8 0.667 8Z"
+                    stroke="#2F323C"
+                    strokeWidth="1.6"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M8 10C9.105 10 10 9.105 10 8C10 6.895 9.105 6 8 6C6.896 6 6 6.895 6 8C6 9.105 6.896 10 8 10Z"
+                    stroke="#2F323C"
+                    strokeWidth="1.6"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </g>
+                <defs>
+                  <clipPath id="clip0">
+                    <rect width="16" height="16" fill="white" />
+                  </clipPath>
+                </defs>
+              </svg>
+            );
+          } else {
+            return (
+              <svg
+                onClick={handleClickDelete}
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M12 4L4 12M4 4L12 12"
+                  stroke="#2F323C"
+                  strokeWidth="1.6"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            );
+          }
+        default:
+          return (
+            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <g clipPath="url(#clip0)">
+                <path
+                  d="M0.667 8C0.667 8 3.333 2.667 8 2.667C12.667 2.667 15.333 8 15.333 8C15.333 8 12.667 13.333 8 13.333C3.333 13.333 0.667 8 0.667 8Z"
+                  stroke="#2F323C"
+                  strokeWidth="1.6"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M8 10C9.105 10 10 9.105 10 8C10 6.895 9.105 6 8 6C6.896 6 6 6.895 6 8C6 9.105 6.896 10 8 10Z"
+                  stroke="#2F323C"
+                  strokeWidth="1.6"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </g>
+              <defs>
+                <clipPath id="clip0">
+                  <rect width="16" height="16" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+          );
+      }
+    }
+  };
+
+  const getBorderClass = () => {
+    if (state === 'focus') return 'border-primary200Main';
+    if (state === 'typing') return 'border-grey700Black';
+    if (status === 'error') return 'border-error';
+    if (status === 'success') return 'border-grey700Black';
+    return 'border-grey200';
+  };
+
   return (
-    <input
-      id={id}
-      type={type}
-      value={value}
-      className={`px-[16px] py-[12px] border text-grey200 rounded-lg shadow-md focus:outline-none focus:border-2 focus:border-primary200Main ${className}`}
-      {...props}
-    />
+    <div className={`relative ${className}`}>
+      <input
+        id={id}
+        type={isPasswordVisible && type === 'password' ? 'text' : type}
+        value={value}
+        onChange={onChange}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        className={`px-[16px] py-[12px] border rounded-lg shadow-md focus:outline-none ${getBorderClass()}`}
+        {...props}
+      />
+      <span className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer">{renderIcon()}</span>
+    </div>
   );
 };
 
