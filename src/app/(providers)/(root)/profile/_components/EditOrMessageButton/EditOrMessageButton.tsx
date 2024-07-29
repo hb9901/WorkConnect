e5f49ef0827f0a8d0cc6a2f26e734b1044570582
hb9901 <hb9901@gmail.com>
2024-07-30 00:52:49 +0900

    style: 메시지 로고 추가

--- src/app/(providers)/(root)/profile/_components/EditOrMessageButton/EditOrMessageButton.tsx
+++ src/app/(providers)/(root)/profile/_components/EditOrMessageButton/EditOrMessageButton.tsx
@@ -1,4 +1,5 @@
 import Button from '@/components/Button';
+import MessageCircleIcon from '@/icons/MessageCircle.svg';
 import { useRouter } from 'next/navigation';
 
 interface EditOrMessageButtonProsp {
@@ -15,9 +16,8 @@ const EditOrMessageButton = ({ isMyPage }: EditOrMessageButtonProsp) => {
   };
 
   return (
-    // button에 icon 넣고 싶음
     <Button theme="primary" isFullWidth={true} onClick={handleClick}>
-      {/* {isMyPage ? '' : <MessageCircleIcon />} */}
+      {isMyPage ? '' : <MessageCircleIcon />}
       {isMyPage ? '프로필 편집' : '메시지 보내기'}
     </Button>
   );
