17d029d8258c0feeaace3659f623a7e209758cc5
Yerin <dev.yerin@gmail.com>
2024-07-29 22:34:10 +0900

    feat: 채팅 상세 리소스 업로드 구현

--- src/app/(providers)/(root)/chat/[id]/page.tsx
+++ src/app/(providers)/(root)/chat/[id]/page.tsx
@@ -8,6 +8,7 @@ import ChatMessages from './_components/ChatMessages';
 import ChatFooter from './_components/ChatFooter';
 import ChatMessagesWrapper from './_components/ChatMessagesWrapper';
 import type { GetChatMessageType } from '@/types/chat';
+import MenuIcon from '@/icons/menu.svg';
 
 // TODO: 데이터 추가 시 수정 필요
 const WORKSPACE_USER_ID = '2b5cc93d-1353-4adb-a8c5-60855dc4e5a2';
@@ -19,6 +20,7 @@ type RealtimePayloadMessagesType = GetChatMessageType & {
 const ChatDetailPage = ({ params }: { params: { id: string } }) => {
   const containerRef = useRef<HTMLDivElement>(null);
   const [payloadMessages, setPayloadMessages] = useState<RealtimePayloadMessagesType[]>([]);
+  const [isOpenUtil, setIsOpenUtil] = useState(false);
 
   const { data: chatMessages = [], isPending: isPendingChatMessages } = useGetChatMessages({
     channel_id: Number(params.id)
@@ -34,6 +36,10 @@ const ChatDetailPage = ({ params }: { params: { id: string } }) => {
     setPayloadMessages((prev) => [...prev, payload.new]);
   };
 
+  const handleOpenUtil = () => {
+    setIsOpenUtil((prev) => !prev);
+  };
+
   //TODO 유저 정보 바뀌면~ 감지하자...
   //QUERY_KEYS.USERS_IN_CHANNEL(channel_id, workspace_user_id)
   useEffect(() => {
@@ -47,13 +53,26 @@ const ChatDetailPage = ({ params }: { params: { id: string } }) => {
   if (isPending) return <div>Loading...</div>;
 
   return (
-    <div className="h-dvh flex flex-col">
-      <TestHeader title="채팅 상세" />
-      <ChatMessagesWrapper ref={containerRef}>
-        <ChatMessages data={chatMessages} usersInChannel={usersInChannel} />
-        <ChatMessages data={payloadMessages} usersInChannel={usersInChannel} />
-      </ChatMessagesWrapper>
-      <ChatFooter id={params.id} />
+    <div className="h-dvh flex flex-col overflow-hidden">
+      <TestHeader
+        title="채팅 상세"
+        rightButton={
+          <button type="button">
+            <MenuIcon />
+          </button>
+        }
+      />
+      <div
+        className={`flex flex-col flex-grow h-[calc(100dvh+35px)] transform ease-in-out duration-300 ${
+          isOpenUtil ? 'translate-y-[-96px]' : 'translate-y-[0px]'
+        }`}
+      >
+        <ChatMessagesWrapper ref={containerRef}>
+          <ChatMessages data={chatMessages} usersInChannel={usersInChannel} />
+          <ChatMessages data={payloadMessages} usersInChannel={usersInChannel} />
+        </ChatMessagesWrapper>
+        <ChatFooter id={params.id} handleOpenUtil={handleOpenUtil} />
+      </div>
     </div>
   );
 };
