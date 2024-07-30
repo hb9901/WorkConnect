6d134a93df7101d5e287fa76657eea04bd4f1d24
Yerin <dev.yerin@gmail.com>
2024-07-31 04:21:24 +0900

    feat: 채팅방 사이드바

--- src/app/(providers)/(root)/[workspaceId]/chat/[id]/page.tsx
+++ src/app/(providers)/(root)/[workspaceId]/chat/[id]/page.tsx
@@ -1,87 +1,36 @@
 'use client';
 
-import { useEffect, useMemo, useRef, useState } from 'react';
-import { subscribeToChat } from '../_utils/subscribe';
-import { useGetChatMessages, useGetUsersInChannel } from '../_hooks/useQueryChat';
-import TestHeader from '../_components/TestHeader';
-import ChatMessages from './_components/ChatMessages';
-import ChatFooter from './_components/ChatFooter';
-import ChatMessagesWrapper from './_components/ChatMessagesWrapper';
-import type { GetChatMessageType } from '@/types/chat';
+import { useState } from 'react';
+import ChatDetail from './_components/ChatDetail';
+import Sidebar from './_components/Sidebar';
 import MenuIcon from '@/icons/menu.svg';
-import { QUERY_KEYS } from '../_constants/constants';
-import { useQueryClient } from '@tanstack/react-query';
+import TestHeader from '../_components/TestHeader';
 
-// TODO: 데이터 추가 시 수정 필요
-const WORKSPACE_USER_ID = '2b5cc93d-1353-4adb-a8c5-60855dc4e5a2';
+const ChatDetailPage = () => {
+  const [isOpenSidebar, setIsOpenSidebar] = useState(false);
 
-type RealtimePayloadMessagesType = GetChatMessageType & {
-  channel_id: string;
-};
-
-const ChatDetailPage = ({ params }: { params: { id: string } }) => {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const [payloadMessages, setPayloadMessages] = useState<RealtimePayloadMessagesType[]>([]);
-  const [isOpenUtil, setIsOpenUtil] = useState(false);
-  const queryClient = useQueryClient();
-
-  const { data: chatMessages = [], isPending: isPendingChatMessages } = useGetChatMessages({
-    channel_id: Number(params.id)
-  });
-  const { data: usersInChannel = {}, isPending: isPendingUsersInChannel } = useGetUsersInChannel({
-    channel_id: Number(params.id),
-    workspace_user_id: WORKSPACE_USER_ID
-  });
-
-  const isPending = isPendingChatMessages || isPendingUsersInChannel;
-
-  const handleInserts = (payload: { new: RealtimePayloadMessagesType }) => {
-    setPayloadMessages((prev) => [...prev, payload.new]);
+  const handleOpenSidebar = () => {
+    setIsOpenSidebar((prev) => !prev);
   };
-
-  const handleUserUpdates = () => {
-    queryClient.invalidateQueries({ queryKey: QUERY_KEYS.USERS_IN_CHANNEL });
-  };
-
-  const handleOpenUtil = () => {
-    setIsOpenUtil((prev) => !prev);
-  };
-
-  const userIds = useMemo(() => {
-    return Object.keys(usersInChannel).join(',');
-  }, [usersInChannel]);
-
-  useEffect(() => {
-    if (!containerRef.current) return;
-
-    containerRef.current.scrollIntoView({ block: 'end' });
-  }, [isPending, payloadMessages]);
-
-  useEffect(subscribeToChat({ handleInserts, handleUserUpdates, id: params.id, userIds }), [userIds]);
-
-  if (isPending) return <div>Loading...</div>;
 
   return (
     <div className="h-dvh flex flex-col overflow-hidden">
       <TestHeader
         title="채팅 상세"
         rightButton={
-          <button type="button">
+          <button type="button" onClick={handleOpenSidebar}>
             <MenuIcon />
           </button>
         }
       />
+      <ChatDetail />
       <div
-        className={`flex flex-col flex-grow h-[calc(100dvh+35px)] transform ease-in-out duration-300 ${
-          isOpenUtil ? 'translate-y-[-96px]' : 'translate-y-[0px]'
-        }`}
-      >
-        <ChatMessagesWrapper ref={containerRef}>
-          <ChatMessages data={chatMessages} usersInChannel={usersInChannel} />
-          <ChatMessages data={payloadMessages} usersInChannel={usersInChannel} />
-        </ChatMessagesWrapper>
-        <ChatFooter id={params.id} handleOpenUtil={handleOpenUtil} />
-      </div>
+        className={`w-full h-full bg-[#333] z-20 fixed top-0 left-0 transition-opacity duration-300 ${isOpenSidebar ? 'opacity-70' : 'opacity-0 pointer-events-none'}`}
+        onClick={handleOpenSidebar}
+      />
+      <Sidebar
+        className={`transition-transform duration-300 will-change-transform ${isOpenSidebar ? '-translate-x-0' : 'translate-x-[100%]'}`}
+      />
     </div>
   );
 };
