d7aa91b67fe28f89b3f2e9e75af7d3854aaab218
alsrhs98 <alsrhs98@gmail.com>
2024-08-02 08:16:33 +0900

    remove: 필요없는 파일 삭제

--- src/app/(providers)/(root)/[workspaceId]/video-channel/_constants/contants.ts
+++
@@ -1,7 +0,0 @@
-export const CONST = {
-  FAKE_WORKSPACE_USER_ID: 'c6b7ba4b-26c2-470f-8332-5e17401134ef',
-  FAKE_WORKSPACE_ID: 2
-};
-
-// c6b7ba4b-26c2-470f-8332-5e17401134ef  민곤 workspaceId
-// 99d81189-d708-45cd-9bc6-7ed773cfccde  형빈 workspaceId
