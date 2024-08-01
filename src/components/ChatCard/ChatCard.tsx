831e900d905fc33a9877c7e250b7ae3e83026300
Yerin <dev.yerin@gmail.com>
2024-08-02 05:59:16 +0900

    fix: ChatCard text-overflow 설정

--- src/components/ChatCard/ChatCard.tsx
+++ src/components/ChatCard/ChatCard.tsx
@@ -16,9 +16,9 @@ export interface ChatCardProps {
 const ChatCard = ({ icon, name, status, date, message, unreadCount, pin, userCount, ...props }: ChatCardProps) => {
   return (
     <div className="flex items-start justify-between p-4 border-b border-white bg-transparent w-full" {...props}>
-      <div className="flex flex-row w-full">
-        <div className="flex items-center justify-center mr-[12px] ">{icon}</div>
-        <div className="flex flex-col">
+      <div className="flex flex-row w-full gap-x-3">
+        <div className="flex items-center justify-center flex-shrink-0">{icon}</div>
+        <div className="flex flex-col whitespace-nowrap overflow-hidden text-ellipsis">
           <div className="flex flex-row mb-[8px]">
             <Typography variant="Title18px" color="grey700Black" className="mr-2">
               {name}
@@ -56,11 +56,11 @@ const ChatCard = ({ icon, name, status, date, message, unreadCount, pin, userCou
               )}
             </Typography>
           </div>
-          <Typography variant="Subtitle16px" color="grey400">
+          <Typography variant="Subtitle16px" color="grey400" className="overflow-hidden text-ellipsis">
             {message}
           </Typography>
         </div>
-        <div className="flex ml-auto">
+        <div className="flex ml-auto flex-shrink-0">
           <div className="flex flex-col ml-auto">
             <Typography variant="Body12px" color="grey300" className="mb-[14px]">
               {date}
