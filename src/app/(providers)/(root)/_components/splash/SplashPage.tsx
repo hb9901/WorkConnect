ff21d078485357365abb57e1e435378aa028d1f1
Yerin <dev.yerin@gmail.com>
2024-08-02 06:43:36 +0900

    fix: 스플래시 화면은 인트로 페이지에서만 노출되고 한번 본 이후에는 보여지지 않는다

--- src/app/(providers)/(root)/_components/splash/SplashPage.tsx
+++ src/app/(providers)/(root)/_components/splash/SplashPage.tsx
@@ -1,28 +1,31 @@
 'use client';
+
 import WorkConnectLogoIcon from '@/icons/WorkConnectLogo.svg';
 import WorkConnectLogoTextIcon from '@/icons/WorkConnectLogoText.svg';
 import { usePathname } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
 const SplashPage = () => {
-  const [fadeOut, setFadeOut] = useState<boolean>(false);
-  const [hidden, setHidden] = useState<boolean>(false);
-  const [isSplashSeen, setIsSplashSeen] = useState<boolean>(false);
-
+  const [isFadeOut, setIsFadeOut] = useState<boolean>(false);
+  const [isHidden, setIsHidden] = useState<boolean>(false);
   const pathname = usePathname();
+  const isSplashSeen = isHidden || pathname !== '/';
 
   useEffect(() => {
-    if (pathname !== '/' && hidden) return;
+    if (isSplashSeen) return;
+
     const timer = setTimeout(() => {
-      setFadeOut(true);
-      setTimeout(() => setHidden(true), 500);
+      setIsFadeOut(true);
+      setTimeout(() => setIsHidden(true), 500);
     }, 2500);
     return () => clearTimeout(timer);
   }, [pathname]);
 
+  if (isSplashSeen) return null;
+
   return (
     <main
-      className={`fixed inset-0 z-50 flex justify-center bg-white ${fadeOut ? 'opacity-0 transition-opacity duration-500' : ''} ${hidden ? 'hidden' : ''}`}
+      className={`fixed inset-0 z-50 flex justify-center bg-white ${isFadeOut ? 'opacity-0 transition-opacity duration-500' : ''} ${isHidden ? 'hidden' : ''}`}
     >
       <div className="flex flex-col justify-center items-center w-[375px] h-dvh">
         <div className="flex flex-col items-center justify-center">
