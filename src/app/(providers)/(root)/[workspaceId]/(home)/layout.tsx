0e096cb1e01259272f818b3bcf69095b21ef684d
hb9901 <hb9901@gmail.com>
2024-08-03 20:01:13 +0900

    fix: header query 사용

--- src/app/(providers)/(root)/[workspaceId]/(home)/layout.tsx
+++ src/app/(providers)/(root)/[workspaceId]/(home)/layout.tsx
@@ -1,45 +1,27 @@
 'use client';
 
-import api from '@/api';
 import { PageLayout } from '@/components/PageLayout';
 import SelectHeader from '@/components/SelectHeader';
 import useWorkspaceId from '@/hooks/useWorkspaceId';
+import useWorkspaceList from '@/hooks/useWorkspaceList';
 import useUserStore from '@/store/userStore';
 import { StrictPropsWithChildren } from '@/types/common';
-import { TWorkspaceInfo } from '@/types/workspace';
-import { useEffect, useState } from 'react';
-import { useShallow } from 'zustand/react/shallow';
 
 const HomeLayout = ({ children }: StrictPropsWithChildren) => {
   const workspaceId = useWorkspaceId();
-  const [workspaceList, setWorkspaceList] = useState<TWorkspaceInfo[] | undefined>();
-  const { userId, setWorkspaceData } = useUserStore(
-    useShallow((state) => ({
-      userId: state.userId,
-      setWorkspaceData: state.setWorkspaceData
-    }))
-  );
-  const getWorkspacaeList = async (workspaceId: number, userId: string) => {
-    const data = await api.workspaceList.getWorkspaceList(workspaceId, userId);
-    const workspaceUserId = data.userData[0].id;
-    const workspaceList = data.workspaceListData;
-    setWorkspaceData(workspaceUserId, workspaceList);
-    setWorkspaceList(workspaceList);
-  };
-  useEffect(() => {
-    if (workspaceId && userId) getWorkspacaeList(workspaceId, userId);
-  }, [userId, workspaceId]);
+  const userId = useUserStore((state) => state.userId);
+  const { workspaceInfo, isPending, isError } = useWorkspaceList(workspaceId, userId);
 
-  if (!(workspaceId && workspaceList))
+  if (isPending || isError || !workspaceInfo) {
     return (
       <PageLayout title="" showTopBar={false}>
         <></>
       </PageLayout>
     );
-
+  }
   return (
     <PageLayout title="" showTopBar={false}>
-      <SelectHeader workspaceList={workspaceList} workspaceId={workspaceId} />
+      <SelectHeader workspaceList={workspaceInfo.workspaceListData} workspaceId={workspaceId} />
       {children}
     </PageLayout>
   );
