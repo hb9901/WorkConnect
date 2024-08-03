d1300b05e51b5237c455b513c25afeac5207793d
Yerin <dev.yerin@gmail.com>
2024-08-03 14:23:36 +0900

    feat: Splash 페이지 추가

--- src/app/(providers)/(root)/layout.tsx
+++ src/app/(providers)/(root)/layout.tsx
@@ -1,7 +1,13 @@
 import { StrictPropsWithChildren } from '@/types/common';
+import Splash from './_components/Splash';
 
 const RootLayout = ({ children }: StrictPropsWithChildren) => {
-  return <>{children}</>;
+  return (
+    <>
+      {children}
+      <Splash />
+    </>
+  );
 };
 
 export default RootLayout;
