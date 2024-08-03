99745909700a62dfd0a70753df77d8ae4b0b06fe
Yerin <dev.yerin@gmail.com>
2024-08-04 06:19:48 +0900

    style: 스크롤바 숨기기

--- src/components/TopSelect/TopSelect.tsx
+++ src/components/TopSelect/TopSelect.tsx
@@ -14,8 +14,8 @@ const TopSelect = ({ workspaceList, isOpen, onClick }: TopSelectProps) => {
     <div className={`fixed top-[50px] z-10 inset-0 ${isOpen ? 'h-full' : 'h-0'}`}>
       {isOpen && <div className="fixed top-[52px] inset-0 bg-black opacity-40" onClick={onClick} />}
       <div
-        className={`fixed top-[50px] left-0 right-0 bg-white transition-all duration-200 
-          ${isOpen ? 'max-h-[172px]' : 'max-h-0'}           
+        className={`fixed top-[50px] left-0 right-0 bg-white transition-all duration-200 scrollbar-hide 
+          ${isOpen ? 'max-h-[172px]' : 'max-h-0'}
           rounded-b-[6px] overflow-y-scroll snap-none`}
       >
         <div className="flex flex-col p-[12px] gap-[16px]">
