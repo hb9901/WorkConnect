2d95ad223061077d0a67d748fd20eeb6c3de3d25
hb9901 <hb9901@gmail.com>
2024-08-03 16:51:24 +0900

    fix: todolist 헤더 수정

--- src/app/(providers)/(root)/[workspaceId]/to-do-list/(providers)/(home)/layout.tsx
+++ src/app/(providers)/(root)/[workspaceId]/to-do-list/(providers)/(home)/layout.tsx
@@ -1,8 +1,21 @@
+'use client';
 import { PageLayout } from '@/components/PageLayout';
+import SelectHeader from '@/components/SelectHeader';
+import useUserStore from '@/store/userStore';
+import { useShallow } from 'zustand/react/shallow';
 
 function TodoListHomeLayout({ children }: { children: React.ReactNode }) {
+  const { workspaceId, workspaceList } = useUserStore(
+    useShallow((state) => ({
+      workspaceId: state.workspaceId,
+      workspaceList: state.workspaceList
+    }))
+  );
+
+  if (!(workspaceId && workspaceList)) return;
   return (
     <PageLayout title="" showTopBar={false}>
+      <SelectHeader workspaceList={workspaceList} workspaceId={workspaceId} />
       {children}
     </PageLayout>
   );
