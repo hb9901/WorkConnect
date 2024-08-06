556185a84e96eae2751d162356228350dee9d447
Yerin <dev.yerin@gmail.com>
2024-08-06 23:42:31 +0900

    feat: Auth 토큰 만료 비교 제거

--- src/app/(providers)/layout.tsx
+++ src/app/(providers)/layout.tsx
@@ -1,27 +1,16 @@
-import AuthProvider from '@/providers/AuthProvider';
 import { AuthStoreProvider } from '@/providers/AuthStoreProvider';
 import QueryProvider from '@/providers/QueryProvider';
 import { SnackBarContextProvider } from '@/providers/SnackBarContext';
 import SupabaseProvider from '@/providers/SupabaseProvider';
 import type { StrictPropsWithChildren } from '@/types/common';
-import { createClient } from '@/utils/supabase/supabaseServer';
 
 const ProvidersLayout = async ({ children }: StrictPropsWithChildren) => {
-  const supabase = createClient();
-  const {
-    data: { session }
-  } = await supabase.auth.getSession();
-
-  const accessToken = session?.access_token;
-
   return (
     <QueryProvider>
       <SupabaseProvider>
-        <AuthProvider accessToken={accessToken || ''}>
-          <AuthStoreProvider>
-            <SnackBarContextProvider>{children}</SnackBarContextProvider>
-          </AuthStoreProvider>
-        </AuthProvider>
+        <AuthStoreProvider>
+          <SnackBarContextProvider>{children}</SnackBarContextProvider>
+        </AuthStoreProvider>
       </SupabaseProvider>
     </QueryProvider>
   );
