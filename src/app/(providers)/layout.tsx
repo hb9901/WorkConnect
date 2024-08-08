52d5d8e2d6590513c77e8e5203c16051ed53bddd
Yerin <dev.yerin@gmail.com>
2024-08-08 13:07:52 +0900

    Revert "feat: Auth 토큰 만료 비교 제거"
    This reverts commit 650ef629820b3400ecd4a700a25d7c60eb523e07.

--- src/app/(providers)/layout.tsx
+++ src/app/(providers)/layout.tsx
@@ -1,16 +1,27 @@
+import AuthProvider from '@/providers/AuthProvider';
 import { AuthStoreProvider } from '@/providers/AuthStoreProvider';
 import QueryProvider from '@/providers/QueryProvider';
 import { SnackBarContextProvider } from '@/providers/SnackBarContext';
 import SupabaseProvider from '@/providers/SupabaseProvider';
 import type { StrictPropsWithChildren } from '@/types/common';
+import { createClient } from '@/utils/supabase/supabaseServer';
 
 const ProvidersLayout = async ({ children }: StrictPropsWithChildren) => {
+  const supabase = createClient();
+  const {
+    data: { session }
+  } = await supabase.auth.getSession();
+
+  const accessToken = session?.access_token;
+
   return (
     <QueryProvider>
       <SupabaseProvider>
-        <AuthStoreProvider>
-          <SnackBarContextProvider>{children}</SnackBarContextProvider>
-        </AuthStoreProvider>
+        <AuthProvider accessToken={accessToken || ''}>
+          <AuthStoreProvider>
+            <SnackBarContextProvider>{children}</SnackBarContextProvider>
+          </AuthStoreProvider>
+        </AuthProvider>
       </SupabaseProvider>
     </QueryProvider>
   );
