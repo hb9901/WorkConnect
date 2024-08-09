149a4bd2ee1bc04804729927deabef0588c0609d
hb9901 <hb9901@gmail.com>
2024-08-09 16:41:42 +0900

    style: todo 날짜 선택 반응형 디자인 구현

---
+++ src/app/(providers)/(root)/[workspaceId]/to-do-list/(providers)/_components/PcDate/PcDate.tsx
@@ -0,0 +1,31 @@
+import Typography from '@/components/Typography';
+import useDateStore from '@/store/dateStore';
+import { Dayjs } from 'dayjs';
+
+interface PcDateProps {
+  weekName: string;
+  date: Dayjs;
+  isSelected: boolean;
+}
+const PcDate = ({ weekName, date, isSelected }: PcDateProps) => {
+  const handleClickDate = useDateStore((state) => state.handleClickDate);
+
+  return (
+    <button
+      onClick={() => handleClickDate(date)}
+      className={`hidden lg:flex lg:flex-row lg:items-center lg:justify-center lg:h-[93px] lg:w-full ${isSelected ? 'bg-[#EBECFE]' : ''}`}
+      key={date.date()}
+    >
+      <Typography
+        variant="Title18px"
+        color={isSelected ? 'primary200Main' : 'grey700Black'}
+        className="lg:flex lg:gap-[24px]"
+      >
+        <span>{date.date()}</span>
+        <span>{weekName}</span>
+      </Typography>
+    </button>
+  );
+};
+
+export default PcDate;
