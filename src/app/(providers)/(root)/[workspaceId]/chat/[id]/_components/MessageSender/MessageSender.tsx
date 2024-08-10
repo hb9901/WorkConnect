36e9043909db0ed9c554629d3b13db8f6a484db6
Yerin <dev.yerin@gmail.com>
2024-08-10 19:24:31 +0900

    feat: pc에서는 엔터시 바로 전송되게 한다

--- src/app/(providers)/(root)/[workspaceId]/chat/[id]/_components/MessageSender/MessageSender.tsx
+++ src/app/(providers)/(root)/[workspaceId]/chat/[id]/_components/MessageSender/MessageSender.tsx
@@ -1,37 +1,15 @@
-import { useRef } from 'react';
-import { useMutationChatMessage } from '../../../_hooks/useChatMutation';
 import MessageTextarea from '../MessageTextarea';
 import UtilsMenu from '../UtilsMenu';
 import ContextMenu from '../ContextMenu';
-import { useParams } from 'next/navigation';
 
 type MessageSenderProps = {
   handleOpenUtil: () => void;
 };
 
 const MessageSender = ({ handleOpenUtil }: MessageSenderProps) => {
-  const { id: channelId } = useParams();
-  const stringId = Array.isArray(channelId) ? channelId[0] : channelId;
-  const ref = useRef<HTMLTextAreaElement>(null);
-
-  const { mutate: mutateChatMessage } = useMutationChatMessage({
-    channel_id: Number(stringId)
-  });
-
-  const handleSendMessage = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    if (!ref.current?.value || ref.current?.value.trim() === '') return;
-
-    mutateChatMessage({ content: ref.current.value, type: 'text' });
-    ref.current.value = '';
-  };
-
   return (
     <div className="relative z-50">
-      <form onSubmit={handleSendMessage}>
-        <MessageTextarea handleOpenUtil={handleOpenUtil} ref={ref} />
-      </form>
+      <MessageTextarea handleOpenUtil={handleOpenUtil} />
       <UtilsMenu handleOpenUtil={handleOpenUtil} />
       <ContextMenu />
     </div>
--- src/app/(providers)/(root)/[workspaceId]/chat/[id]/_components/MessageTextarea/MessageTextarea.tsx
+++ src/app/(providers)/(root)/[workspaceId]/chat/[id]/_components/MessageTextarea/MessageTextarea.tsx
@@ -1,47 +1,86 @@
+'use client';
+
 import PlusIcon from '@/icons/Plus.svg';
 import SmileIcon from '@/icons/Smile.svg';
 import SendIcon from '@/icons/Send.svg';
-import { forwardRef } from 'react';
+import { useRef, useState } from 'react';
+import { useParams } from 'next/navigation';
+import { useMutationChatMessage } from '../../../_hooks/useChatMutation';
 
 type MessageTextareaProps = {
   handleOpenUtil: () => void;
 };
 
-const MessageTextarea = forwardRef<HTMLTextAreaElement, MessageTextareaProps>(({ handleOpenUtil }, ref) => {
-  const handleResizeHeight = () => {
-    const textArea = ref as React.RefObject<HTMLTextAreaElement>;
-    if (!textArea.current) return;
-    textArea.current.style.height = 'auto';
-    textArea.current.style.height = textArea.current.scrollHeight + 'px';
+const handleResizeHeight = (textArea: HTMLTextAreaElement | null) => {
+  if (!textArea) return;
+  textArea.style.height = 'auto';
+  textArea.style.height = textArea.scrollHeight + 'px';
+};
+
+const MessageTextarea = ({ handleOpenUtil }: MessageTextareaProps) => {
+  const [isComposing, setIsComposing] = useState(false);
+
+  const { id: channelId } = useParams();
+  const stringId = Array.isArray(channelId) ? channelId[0] : channelId;
+
+  const ref = useRef<HTMLTextAreaElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  const { mutate: mutateChatMessage } = useMutationChatMessage({
+    channel_id: Number(stringId)
+  });
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!ref.current?.value || ref.current?.value.trim() === '') return;
+
+    mutateChatMessage({ content: ref.current.value, type: 'text' });
+    ref.current.value = '';
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    handleResizeHeight(ref.current);
+
+    if (isComposing) return;
+    if (window.innerWidth < 1024) return;
+    if (!(event.key === 'Enter' && !event.shiftKey)) return;
+
+    event.preventDefault();
+    buttonRef.current?.click();
   };
 
   return (
-    <div className="flex py-4 w-full bg-white p-4 pb-[14px] gap-[10px] border-t border-grey50 items-center">
-      <button
-        type="button"
-        onClick={handleOpenUtil}
-        aria-label="파일 추가"
-        className="w-6 h-6 rounded-full bg-primary200Main flex justify-center items-center shrink-0"
-      >
-        <PlusIcon className="w-5 h-5 text-white stroke-current" />
-      </button>
-      <div className="relative w-full">
-        <textarea
-          ref={ref}
-          rows={1}
-          onKeyDown={handleResizeHeight}
-          placeholder="메시지를 입력하세요"
-          className="rounded w-full px-2 py-[12px] bg-[#fafafa] text-[12px] leading-[130%] tracking-[-0.24px] pr-9 resize-none h-[40px] max-h-[86px] block"
-        />
-        <button type="button" className="absolute right-2 bottom-[10px] z-10" aria-label="이모지 선택">
-          <SmileIcon className="w-5 h-5 text-grey400" />
+    <form onSubmit={handleSubmit}>
+      <div className="flex py-4 w-full bg-white p-4 pb-[14px] gap-[10px] border-t border-grey50 items-center">
+        <button
+          type="button"
+          onClick={handleOpenUtil}
+          aria-label="파일 추가"
+          className="w-6 h-6 rounded-full bg-primary200Main flex justify-center items-center shrink-0"
+        >
+          <PlusIcon className="w-5 h-5 text-white stroke-current" />
+        </button>
+        <div className="relative w-full">
+          <textarea
+            ref={ref}
+            rows={1}
+            onKeyDown={handleKeyDown}
+            placeholder="메시지를 입력하세요"
+            className="rounded w-full px-2 py-[12px] bg-[#fafafa] text-[12px] leading-[130%] tracking-[-0.24px] pr-9 resize-none h-[40px] max-h-[86px] block"
+            onCompositionStart={() => setIsComposing(true)}
+            onCompositionEnd={() => setIsComposing(false)}
+          />
+          <button type="button" className="absolute right-2 bottom-[10px] z-10" aria-label="이모지 선택">
+            <SmileIcon className="w-5 h-5 text-grey400" />
+          </button>
+        </div>
+        <button ref={buttonRef} type="submit" aria-label="전송">
+          <SendIcon className="w-5 h-5 text-grey700Black" />
         </button>
       </div>
-      <button type="submit" aria-label="전송">
-        <SendIcon className="w-5 h-5 text-grey700Black" />
-      </button>
-    </div>
+    </form>
   );
-});
+};
 
 export default MessageTextarea;
