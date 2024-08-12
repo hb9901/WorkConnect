ec4f5777ddb8cb9202100fbf02a07837772be4c0
hb9901 <hb9901@gmail.com>
2024-08-12 11:57:43 +0900

    style: todolist 배경 임시 제작

--- src/app/(providers)/(root)/[workspaceId]/to-do-list/(providers)/_components/PcDate/PcDate.tsx
+++ src/app/(providers)/(root)/[workspaceId]/to-do-list/(providers)/_components/PcDate/PcDate.tsx
@@ -11,20 +11,23 @@ const PcDate = ({ weekName, date, isSelected }: PcDateProps) => {
   const handleClickDate = useDateStore((state) => state.handleClickDate);
 
   return (
-    <button
-      onClick={() => handleClickDate(date)}
-      className={`hidden lg:flex lg:flex-row lg:items-center lg:justify-center lg:h-[93px] lg:w-full ${isSelected ? 'bg-[#EBECFE]' : ''}`}
-      key={date.date()}
-    >
-      <Typography
-        variant="Title18px"
-        color={isSelected ? 'primary200Main' : 'grey700Black'}
-        className="lg:flex lg:gap-[24px]"
+    <>
+      <div className="lg:absolute lg:top-[30px] lg:bottom-0 lg:left-0 lg:right-[calc(100%-297px)] lg:bg-[#F4F4F6] lg:-z-[1]"></div>
+      <button
+        onClick={() => handleClickDate(date)}
+        className={`hidden lg:flex lg:flex-row lg:items-center lg:justify-center lg:h-[93px] lg:w-full ${isSelected ? 'bg-[#EBECFE]' : ''}`}
+        key={date.date()}
       >
-        <span>{date.date()}</span>
-        <span>{weekName}</span>
-      </Typography>
-    </button>
+        <Typography
+          variant="Title18px"
+          color={isSelected ? 'primary200Main' : 'grey700Black'}
+          className="lg:flex lg:gap-[24px]"
+        >
+          <span>{date.date()}</span>
+          <span>{weekName}</span>
+        </Typography>
+      </button>
+    </>
   );
 };
 
