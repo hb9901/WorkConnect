74179dc8ca1a72e2685d8a1a4cde97c255f83dd7
riverSun1 <haeweon0426@gmail.com>
2024-08-13 17:13:24 +0900

    feat: FileEmpty 컴포넌트

---
+++ src/app/(providers)/(root)/test/page.tsx
@@ -0,0 +1,11 @@
+import FileEmpty from '@/components/FileEmpty';
+
+const page = () => {
+  return (
+    <div>
+      <FileEmpty className="mt-[40px]" />
+    </div>
+  );
+};
+
+export default page;
