f478e416b27f408862e949d35ff59b6b2a9ef562
hb9901 <hb9901@gmail.com>
2024-08-14 00:09:53 +0900

    fix: parallel 라우팅 layout 수정 및 todo 등록 페이지 반응형 기능 구현

--- src/app/(providers)/(root)/[workspaceId]/profile/[targetWorkspaceUserId]/layout.tsx
+++ src/app/(providers)/(root)/[workspaceId]/profile/[targetWorkspaceUserId]/layout.tsx
@@ -1,4 +1,4 @@
-import { PageLayout } from '@/components/PageLayout';
+import BottomNavigationBar from '@/components/BottomNavigationBar';
 import SelectHeader from '@/components/SelectHeader';
 import { StrictPropsWithChildren } from '@/types/common';
 
@@ -14,32 +14,25 @@ const ProfileParallelLayout = ({ children, home, params }: StrictPropsWithChildr
 
   return (
     <>
-      {/*모바일 레이아웃*/}
-      <div className="lg:hidden">
-        <div className="flex lg:flex-row">
-          <div className="hidden lg:flex lg:w-full">{home}</div>
+      <main>
+        <div className="hidden lg:grid">
+          <SelectHeader workspaceId={workspaceId} isFull isHidden />
+        </div>
+        <div className="flex lg:flex-row lg:pl-[87px]">
+          <div className="hidden lg:grid lg:w-full ">{home}</div>
           <div className="w-full lg:min-w-[374px] lg:max-w-[374px]"></div>
-          <div className="w-full fixed lg:min-w-[374px] lg:max-w-[374px] lg:w-[374px] lg:top-[84px] lg:right-0">
+          <div
+            className="w-full fixed lg:min-w-[374px] lg:max-w-[374px] lg:w-[374px] lg:top-[84px] lg:right-0 
+            lg:border-[#E5E7EB] lg:border-l-[1px]"
+          >
             {children}
           </div>
         </div>
-      </div>
-      {/*PC 레이아웃*/}
-      <div className="hidden lg:grid">
-        <PageLayout title="" showTopBar={false}>
-          <SelectHeader workspaceId={workspaceId} isFull isHidden />
-          <div className="flex lg:flex-row lg:pl-[87px]">
-            <div className="hidden lg:grid lg:w-full ">{home}</div>
-            <div className="w-full lg:min-w-[374px] lg:max-w-[374px]"></div>
-            <div
-              className="w-full fixed lg:min-w-[374px] lg:max-w-[374px] lg:w-[374px] lg:top-[84px] lg:right-0 
-            lg:border-[#E5E7EB] lg:border-l-[1px]"
-            >
-              {children}
-            </div>
-          </div>
-        </PageLayout>
-      </div>
+      </main>
+      <>
+        <div className="h-[87px]" />
+        <BottomNavigationBar className="hidden fixed bottom-0 left-0 z-30 xs:w-full lg:top-[84px] lg:flex lg:items-start lg:w-[87px] lg:h-[vh100%] lg:bg-[#F4F4F6] lg:border-r-[1px] lg:border-grey50" />
+      </>
     </>
   );
 };
--- src/app/(providers)/(root)/[workspaceId]/to-do-list/add/[id]/layout.tsx
+++ src/app/(providers)/(root)/[workspaceId]/to-do-list/add/[id]/layout.tsx
@@ -1,4 +1,4 @@
-import { PageLayout } from '@/components/PageLayout';
+import BottomNavigationBar from '@/components/BottomNavigationBar';
 import SelectHeader from '@/components/SelectHeader';
 import { StrictPropsWithChildren } from '@/types/common';
 import PcHeader from '../../_components/PcHeader';
@@ -15,29 +15,27 @@ const TodoAddLayout = ({ children, todolist, params }: StrictPropsWithChildren<T
 
   return (
     <>
-      {/*모바일 레이아웃 */}
-      <div className="lg:hidden">
-        <div className="hidden lg:grid lg:w-full">
+      <main>
+        <div className="hidden lg:grid">
+          <SelectHeader workspaceId={workspaceId} isTodoList />
+        </div>
+
+        <div className="hidden lg:grid lg:w-full lg:pl-[87px]">
           <PcHeader />
           {todolist}
         </div>
-        <div className="w-full">{children}</div>
-      </div>
-      {/*PC 레이아웃 */}
-      <div className="hidden lg:grid">
-        <PageLayout title="" showTopBar={false}>
-          <SelectHeader workspaceId={workspaceId} isTodoList />
-          <div className="hidden lg:grid lg:w-full lg:pl-[87px]">
-            <PcHeader />
-            {todolist}
+        <div className="w-full lg:w-auto lg:absolute lg:inline-grid lg:grid-cols-3 lg:gap-[12px] lg:bottom-0 lg:left-[400px] lg:top-[84px] lg:right-[17px] lg:rounded-[6px]">
+          <div />
+          <div />
+          <div className="lg:h-full lg:w-full lg:bg-white lg:border-[#E5E7EB] lg:border-l-[1px] overflow-y-scroll">
+            {children}
           </div>
-          <div className="hidden lg:absolute lg:inline-grid lg:grid-cols-3 lg:gap-[12px] lg:bottom-0 lg:left-[400px] lg:top-[84px] lg:right-[17px] lg:rounded-[6px]">
-            <div />
-            <div />
-            <div className="lg:h-full lg:w-full lg:bg-white lg:border-[#E5E7EB] lg:border-l-[1px]"> {children}</div>
-          </div>
-        </PageLayout>
-      </div>
+        </div>
+      </main>
+      <>
+        <div className="h-[87px]" />
+        <BottomNavigationBar className="hidden fixed bottom-0 left-0 z-30 xs:w-full lg:top-[84px] lg:flex lg:items-start lg:w-[87px] lg:h-[vh100%] lg:bg-[#F4F4F6] lg:border-r-[1px] lg:border-grey50" />
+      </>
     </>
   );
 };
