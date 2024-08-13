7ba6b5077dca93658dac2762d61fa36917a5b4e9
yerin <dev.yerin@gmail.com>
2024-08-14 00:57:41 +0900

    style: pc 레이아웃 스크롤바 스타일 적용

--- src/app/(providers)/(root)/[workspaceId]/channels/(chat)/[id]/(home)/_components/MessagesContainer/MessageWrapper.tsx
+++ src/app/(providers)/(root)/[workspaceId]/channels/(chat)/[id]/(home)/_components/MessagesContainer/MessageWrapper.tsx
@@ -3,7 +3,7 @@ import { forwardRef } from 'react';
 
 const MessagesWrapper = forwardRef<HTMLDivElement, StrictPropsWithChildren>(({ children }, ref) => {
   return (
-    <article className="flex-grow overflow-y-auto px-4">
+    <article className="flex-grow overflow-y-scroll px-4 scroll-container">
       <div className="relative flex flex-col gap-6 py-4" ref={ref}>
         {children}
       </div>
--- src/app/(providers)/(root)/[workspaceId]/channels/layout.tsx
+++ src/app/(providers)/(root)/[workspaceId]/channels/layout.tsx
@@ -3,8 +3,10 @@ import { StrictPropsWithChildren } from '@/types/common';
 const ChannelListLayout = ({ list, children }: StrictPropsWithChildren<{ list: React.ReactNode }>) => {
   return (
     <div className="lg:pl-[87px] lg:flex">
-      <div className="lg:max-w-[300px] lg:max-h-dvh lg:overflow-y-auto">{list}</div>
-      <div className="lg:flex-1">{children}</div>
+      <div className="w-full lg:max-w-[300px] lg:max-h-dvh lg:overflow-y-scroll lg:flex-shrink-0 scroll-container">
+        {list}
+      </div>
+      <div className="lg:w-[calc(100%-300px)]">{children}</div>
     </div>
   );
 };
