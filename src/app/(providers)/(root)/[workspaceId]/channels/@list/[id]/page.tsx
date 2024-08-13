fb6a4a0b9637850cbc74e10e522274f5b71b4f3b
yerin <dev.yerin@gmail.com>
2024-08-13 21:10:10 +0900

    refactor: 채널에 사용되는 list slot은 최초 접속시 pc일때만 노출된다.

--- src/app/(providers)/(root)/[workspaceId]/channels/@list/[id]/page.tsx
+++ src/app/(providers)/(root)/[workspaceId]/channels/@list/[id]/page.tsx
@@ -1,6 +1,17 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import ChatList from '../_components/ChatList';
 
 const ChatSlot = () => {
+  const [isPC, setIsPC] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    setIsPC(window.innerWidth >= 1024);
+  }, []);
+
+  if (!isPC) return null;
+
   return <ChatList />;
 };
 
