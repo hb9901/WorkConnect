fb101d76a85468272c4c7089dc85dd788e7d6fc4
hb9901 <hb9901@gmail.com>
2024-08-13 15:52:47 +0900

    fix: 헤더, todolist add 페이지 수정

--- src/app/(providers)/(root)/[workspaceId]/to-do-list/_components/TodoListMain/TodoListMain.tsx
+++ src/app/(providers)/(root)/[workspaceId]/to-do-list/_components/TodoListMain/TodoListMain.tsx
@@ -4,6 +4,7 @@ import useDateStore from '@/store/dateStore';
 import useUserStore from '@/store/userStore';
 import DateButtons from '../DateButtons';
 import MonthDate from '../MonthDate';
+import PcBackGround from '../PcBackGround';
 import ToDoList from '../ToDoList/ToDoList';
 import WeekDate from '../WeekDate';
 
@@ -21,15 +22,15 @@ const ToDoListMain = () => {
     <>
       <main className="flex flex-col px-[16px] lg:inline-flex lg:flex-row lg:px-0 lg:h-full lg:w-full">
         <div className="lg:flex lg:flex-col lg:flex-shrink-0 lg:w-[297px] lg:bg-[#F4F4F6]">
-          <div className="lg:absolute lg:top-[84px] lg:bottom-0 lg:left-0 lg:right-[calc(100%-384px)] lg:bg-[#F4F4F6] lg:-z-[1]"></div>
           <DateButtons />
           {isWeekly ? <WeekDate /> : <MonthDate />}
         </div>
-        <div className="flex flex-col lg:inline-grid lg:grid-cols-3 lg:ml-[16px] lg:mr-[17px] lg:mt-[24px] lg:gap-[12px] lg:w-full">
+        <div className="flex flex-col lg:inline-grid lg:grid-cols-3 lg:ml-[16px] lg:mr-[17px] lg:mt-[24px] lg:gap-[12px] lg:w-full lg:h-full">
           <ToDoList title="진행 전" todoList={beforeTodoList} />
           <ToDoList title="진행 중" todoList={progressTodoList} />
           <ToDoList title="완료" todoList={completedTodoList} />
         </div>
+        <PcBackGround />
       </main>
     </>
   );
--- src/app/(providers)/(root)/[workspaceId]/to-do-list/add/[id]/layout.tsx
+++ src/app/(providers)/(root)/[workspaceId]/to-do-list/add/[id]/layout.tsx
@@ -1,7 +1,5 @@
-'use client';
 import { PageLayout } from '@/components/PageLayout';
 import SelectHeader from '@/components/SelectHeader';
-import useWorkspaceId from '@/hooks/useWorkspaceId';
 import { StrictPropsWithChildren } from '@/types/common';
 import PcHeader from '../../_components/PcHeader';
 
@@ -13,7 +11,7 @@ interface ProfileParallelLayoutProps {
 }
 
 const ProfileParallelLayout = ({ children, todolist, params }: StrictPropsWithChildren<ProfileParallelLayoutProps>) => {
-  const workspaceId = useWorkspaceId();
+  const workspaceId = Number(params.workspaceId);
 
   return (
     <>
@@ -23,9 +21,7 @@ const ProfileParallelLayout = ({ children, todolist, params }: StrictPropsWithCh
           <PcHeader />
           {todolist}
         </div>
-        <div className="w-full lg:max-w-[calc((100dvw-297px)/3)] lg:absolute lg:top-[84px] lg:right-0 lg:transition-all lg:duration-200">
-          {children}
-        </div>
+        <div className="w-full">{children}</div>
       </div>
       {/*PC 레이아웃 */}
       <div className="hidden lg:grid">
@@ -35,8 +31,10 @@ const ProfileParallelLayout = ({ children, todolist, params }: StrictPropsWithCh
             <PcHeader />
             {todolist}
           </div>
-          <div className="w-full lg:max-w-[calc((100dvw-297px)/3)] lg:absolute lg:top-[84px] lg:right-0 lg:transition-all lg:duration-200">
-            {children}
+          <div className="hidden lg:absolute lg:inline-grid lg:grid-cols-3 lg:gap-[12px] lg:bottom-0 lg:left-[400px] lg:top-[108px] lg:right-[17px] lg:rounded-[6px]">
+            <div />
+            <div />
+            <div className="lg:h-full lg:w-full lg:bg-white"> {children}</div>
           </div>
         </PageLayout>
       </div>
