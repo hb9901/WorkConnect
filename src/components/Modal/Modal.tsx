7cab278ee65de8ee7f775f4f632ebbd6033c14b3
riverSun1 <haeweon0426@gmail.com>
2024-08-18 22:23:04 +0900

    feat: Modal svg 분리

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,5 +1,6 @@
+import XIcon from '@/icons/X.svg';
+import Image from 'next/image';
 import React from 'react';
-
 export interface ModalProps {
   isOpen: boolean;
   onClose: () => void;
@@ -16,15 +17,7 @@ const Modal = ({ isOpen, onClose, children, isModal = true, ...props }: ModalPro
       <div className="fixed bg-white rounded-lg p-[10px] flex flex-col items-center" {...props}>
         {isModal && (
           <button onClick={onClose} className="self-end text-gray-700 cursor-pointer">
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M18 6L6 18M6 6L18 18"
-                stroke="#020715"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
+            <Image src={XIcon} alt="x" />
           </button>
         )}
         {children}
