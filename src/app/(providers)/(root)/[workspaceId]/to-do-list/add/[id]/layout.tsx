2b4c05f4c0c05b01b3669b27b6fabe04d702c380
hb9901 <hb9901@gmail.com>
2024-08-18 20:10:46 +0900

    fix: date 모달 창 모바일 css 수정

--- src/app/(providers)/(root)/[workspaceId]/to-do-list/add/[id]/layout.tsx
+++ src/app/(providers)/(root)/[workspaceId]/to-do-list/add/[id]/layout.tsx
@@ -21,7 +21,7 @@ const TodoAddLayout = ({ children, todolist }: StrictPropsWithChildren<TodoAddLa
         </PageMain>
         <div
           className="w-full lg:w-auto lg:absolute lg:inline-grid lg:grid-cols-3 
-        lg:gap-[12px] lg:bottom-0 lg:left-[330px] lg:top-[84px] lg:right-0 "
+          lg:bottom-0 lg:left-[360px] lg:top-[84px] lg:right-0 "
         >
           <div />
           <div />
@@ -39,30 +39,3 @@ const TodoAddLayout = ({ children, todolist }: StrictPropsWithChildren<TodoAddLa
 };
 
 export default TodoAddLayout;
-// <>
-//   <main>
-//     <SelectHeader workspaceId={workspaceId} isTodoList isHidden />
-
-//     <div className="hidden lg:grid lg:w-full lg:pl-[87px] lg:max-h-[calc(100dvh-84px)] lg:overflow-y-scroll lg:scroll-container">
-//       <PcHeader />
-//       {todolist}
-//     </div>
-//     <div
-//       className="w-full lg:w-auto lg:absolute lg:inline-grid lg:grid-cols-3
-//     lg:gap-[12px] lg:bottom-0 lg:left-[354px] lg:top-[84px] lg:right-0 "
-//     >
-//       <div />
-//       <div />
-//       <div
-//         className="lg:h-full lg:w-full lg:bg-white lg:border-[#E5E7EB] lg:border-l-[1px]
-//         lg:max-h-[calc(100dvh-84px)] lg:overflow-y-scroll lg:scroll-container"
-//       >
-//         {children}
-//       </div>
-//     </div>
-//   </main>
-//   <>
-//     <div className="h-[87px] lg:hidden" />
-//     <BottomNavigationBar className="hidden fixed bottom-0 left-0 z-30 xs:w-full lg:top-[84px] lg:flex lg:items-start lg:w-[87px] lg:h-[vh100%] lg:bg-[#F4F4F6] lg:border-r-[1px] lg:border-grey50" />
-//   </>
-// </>;
