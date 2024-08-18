3d48716ea069fbbca6ac4c84a2de74dcd0b9b3e0
yerin <dev.yerin@gmail.com>
2024-08-19 00:07:10 +0900

    Revert "feat: realtime 에서 chat 업데이트가 생기면 채팅방 접속 시간을 db에 업데이트"
    This reverts commit 3afb042ba2db36a6953689c53458228b371a41fe.

--- src/app/(providers)/(root)/[workspaceId]/channels/_components/ChannelList/_hooks/useChannelHandlers.ts
+++ src/app/(providers)/(root)/[workspaceId]/channels/_components/ChannelList/_hooks/useChannelHandlers.ts
@@ -1,12 +1,9 @@
-'use client';
-
 import { useQueryClient } from '@tanstack/react-query';
 import { useCallback } from 'react';
 import { updateChatChannels } from '../_utils/updateChatChannels';
 import { ChatSubscribePayloadProps } from '@/types/chat';
 import { GetChannelsResponse } from '@/types/channel';
 import { QUERY_KEYS } from '../../../_constants/constants';
-import { useMutationUpdateChannelActiveAt } from '../../../(chat)/_hook/useChatMutation';
 
 type WorkspaceInfoProps = {
   workspaceId: number;
@@ -14,11 +11,9 @@ type WorkspaceInfoProps = {
 
 export const useChannelHandlers = () => {
   const queryClient = useQueryClient();
-  const { mutate: updateChannelActiveAt } = useMutationUpdateChannelActiveAt();
 
   const handleChatInserts = useCallback(({ workspaceId }: WorkspaceInfoProps) => {
     return ({ new: payload }: { new: ChatSubscribePayloadProps }) => {
-      updateChannelActiveAt(payload.channel_id);
       queryClient.setQueryData(QUERY_KEYS.CHANNELS(workspaceId), (prev: GetChannelsResponse[]) => {
         return updateChatChannels(prev, payload);
       });
