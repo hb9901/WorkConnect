de4d6d630374300659a2a5b454f19a80749ac887
riverSun1 <haeweon0426@gmail.com>
2024-08-20 02:17:41 +0900

    feat: CheckBox storybook 작성

--- src/components/CheckBox/CheckBox.stories.tsx
+++ src/components/CheckBox/CheckBox.stories.tsx
@@ -12,20 +12,20 @@ export default {
     theme: {
       control: 'select',
       options: ['primary', 'grey'],
-      description: '체크박스의 테마 색상',
+      description: '체크박스의 테마 색상을 지정합니다.',
       defaultValue: 'primary'
     },
     children: {
       control: 'text',
-      description: '체크박스 옆에 표시할 텍스트'
+      description: '체크박스 옆에 표시할 텍스트를 지정합니다.'
     },
     onClick: {
       action: 'clicked',
-      description: '체크박스가 클릭될 때 호출되는 함수'
+      description: '체크박스가 클릭될 때 호출되는 함수를 지정합니다.'
     },
     isChecked: {
       control: 'boolean',
-      description: '체크박스의 체크 여부',
+      description: '체크박스의 체크 여부를 지정합니다.',
       defaultValue: false
     }
   }
