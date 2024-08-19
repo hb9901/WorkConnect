dbe00074727055f197f4c81cc9bc70da1220fe3b
riverSun1 <haeweon0426@gmail.com>
2024-08-20 02:16:54 +0900

    feat: CheckBox storybook 작성

--- src/components/CheckBox/CheckBox.stories.tsx
+++ src/components/CheckBox/CheckBox.stories.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import CheckBox from './CheckBox';
+import CheckBox, { CheckBoxProps } from './CheckBox';
 
 export default {
   title: 'Components/CheckBox',
@@ -22,14 +22,19 @@ export default {
     onClick: {
       action: 'clicked',
       description: '체크박스가 클릭될 때 호출되는 함수'
+    },
+    isChecked: {
+      control: 'boolean',
+      description: '체크박스의 체크 여부',
+      defaultValue: false
     }
   }
 };
 
-export const Checkbox = () => {
+export const Checkbox = (args: CheckBoxProps) => {
   const [checked, setChecked] = useState(false);
   return (
-    <CheckBox theme="primary" onClick={() => setChecked((prev) => !prev)} isChecked={checked}>
+    <CheckBox theme={args.theme} onClick={() => setChecked((prev) => !prev)} isChecked={checked}>
       동의
     </CheckBox>
   );
