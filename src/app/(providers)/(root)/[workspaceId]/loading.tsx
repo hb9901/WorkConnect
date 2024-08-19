02c0f65a6f0741c3e68b0e88a59a7ac3d1397c99
hb9901 <hb9901@gmail.com>
2024-08-19 09:42:54 +0900

    style: 로딩 배경색 수정

--- src/app/(providers)/(root)/[workspaceId]/loading.tsx
+++ src/app/(providers)/(root)/[workspaceId]/loading.tsx
@@ -2,7 +2,7 @@ import LoadingSpinner2 from '@/components/LoadingSpinner2';
 
 const Loading = () => {
   return (
-    <div className="absolute top-0 bottom-0 left-0 right-0 bg-red-100">
+    <div className="absolute top-0 bottom-0 left-0 right-0 ">
       <LoadingSpinner2 />
     </div>
   );
