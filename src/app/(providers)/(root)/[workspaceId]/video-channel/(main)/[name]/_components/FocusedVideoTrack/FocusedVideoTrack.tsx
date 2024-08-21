a6f31a9a084dd4e1d58dc34ba94d47dd1c4b3286
alsrhs98 <alsrhs98@gmail.com>
2024-08-21 11:47:04 +0900

    "fix/로딩 삭제"

--- src/app/(providers)/(root)/[workspaceId]/video-channel/(main)/[name]/_components/FocusedVideoTrack/FocusedVideoTrack.tsx
+++ src/app/(providers)/(root)/[workspaceId]/video-channel/(main)/[name]/_components/FocusedVideoTrack/FocusedVideoTrack.tsx
@@ -1,19 +1,19 @@
 import CameraPlaceholderIcon from '@/icons/CameraPlaceholder.svg';
 import { isTrackReference } from '@livekit/components-core';
 import { VideoTrack } from '@livekit/components-react';
+import { Track } from 'livekit-client';
 import React from 'react';
 import useFocosedTrack from '../../_store/useFocusTrack';
 const FocusedVideoTrack = () => {
   const { focusedTrack: focusedTrackRef } = useFocosedTrack();
   return (
     <>
-      {isTrackReference(focusedTrackRef) ? (
+      {isTrackReference(focusedTrackRef) &&
+      (Track.Source.Camera === focusedTrackRef.source || Track.Source.ScreenShare === focusedTrackRef.source) ? (
         <div className={`relative bg-grey700Black rounded-[5px] 'h-[88%]' `}>
-          (Track.Source.Camera === focusedTrackRef.source || Track.Source.ScreenShare === focusedTrackRef.source) && (
           <div>
             <VideoTrack trackRef={focusedTrackRef} />
           </div>
-          )
         </div>
       ) : (
         <div className="bg-grey700Black rounded-lg w-[88%] h-[88%] flex justify-center items-center">
